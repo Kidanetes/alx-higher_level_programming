5aa10da556881d5feadcbae0a2765149448bb4c9
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-03-08 02:58:35 +0300

    python script

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    print("{:d}".format(number % -10 if number < 0 else number % 10), end="")
+    print("{:d}".format(-number % 10 if number < 0 else number % 10), end="")
     if number < 0:
-        return number % -10
+        return -number % 10
     else:
-         return number % 10
+        return number % 10
