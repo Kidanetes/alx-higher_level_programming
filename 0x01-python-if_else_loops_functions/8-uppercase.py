42671a465cf18a4ef5f5d1562eab99bb13331eb0
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-03-08 02:39:25 +0300

    python script

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,8 +2,8 @@
 def uppercase(str):
     for i in range(len(str)):
         if ord(str[i]) >= ord('a') and ord(str[i]) <= ord('z'):
-            print("{:c}".format(ord(str[i]) + 
-                                ord('A') - ord('a')), end="\n" if i == len(str) - 1 else "")
+            letter = str[i]) + ord('A') - ord('a')
         else:
-            print("{:c}".format(ord(str[i])),
-                  end="\n" if i == len(str) - 1 else "")
+            letter = str[i]
+        print("{:c}".format(letter))
+    print("")
