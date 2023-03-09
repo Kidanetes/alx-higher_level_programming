189c84c78c75076fa313144a78da159fbb7347f2
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-03-09 16:38:45 +0300

    python script

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    sum = 0
+    numOfArgs = len(sys.argv) - 1
+    if numOfArgs = 0:
+        print(sum)
+    else:
+        for i in range(1, numOfArgs + 1):
+            sum = sum + int(sys.argv[i])
+        print(sum)
