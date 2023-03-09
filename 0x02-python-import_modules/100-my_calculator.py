9c166bde636c89a23d3d84b3f2a78b1c17eb99d8
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-03-10 02:13:04 +0300

    python script

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -10,19 +10,19 @@ if __name__ == "__main__":
         if sys.argv[2] == "+":
             a = int(sys.argv[1])
             b = int(sys.argv[3])
-            print("{:d} + {:d} = {:d}".format(a, b, add(a ,b)))
+            print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
         elif sys.argv[2] == "-":
             a = int(sys.argv[1])
             b = int(sys.argv[3])
-            print("{:d} - {:d} = {:d}".format(a, b, sub(a ,b)))
+            print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
         elif sys.argv[2] == "*":
             a = int(sys.argv[1])
             b = int(sys.argv[3])
-            print("{:d} * {:d} = {:d}".format(a, b, mul(a ,b)))
+            print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
         elif sys.argv[2] == "/":
             a = int(sys.argv[1])
             b = int(sys.argv[3])
-            print("{:d} / {:d} = {:d}".format(a, b, div(a ,b)))
+            print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
         else:
             print("Unknown operator. Available operators: +, -, * and /")
             sys.exit(1)
