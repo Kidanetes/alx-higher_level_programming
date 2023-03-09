75ae3896c5e79d2ee961aff4beb6bb4383d0c311
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-03-09 16:26:45 +0300

    python script

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
     else:
         print("{:d} arguments:".format(numOfArgs))
     for i in range(1, numOfArgs + 1)
-        print("{:d}: {:s}".format(i, sys.argv[i]))
+        print("{:d}: {}".format(i, sys.argv[i]))
