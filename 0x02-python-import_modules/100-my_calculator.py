620c52966ec78ad3034b1d439d8335d6d6233cd6
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-03-10 02:00:02 +0300

    python script

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,19 +7,19 @@ if __name__ == "__main__":
         print("Usage: .//100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
     else:
-        if sys.argv[2] = "+":
+        if sys.argv[2] == "+":
             a = int(sys.argv[1])
             b = int(sys.argv[3])
             print("{:d} + {:d} = {:d}".format(a, b, add(a ,b)))
-        elif sys.argv[2] = "-":
+        elif sys.argv[2] == "-":
             a = int(sys.argv[1])
             b = int(sys.argv[3])
             print("{:d} - {:d} = {:d}".format(a, b, sub(a ,b)))
-        elif sys.argv[2] = "*":
+        elif sys.argv[2] == "*":
             a = int(sys.argv[1])
             b = int(sys.argv[3])
             print("{:d} * {:d} = {:d}".format(a, b, mul(a ,b)))
-        elif sys.argv[2] = "/":
+        elif sys.argv[2] == "/":
             a = int(sys.argv[1])
             b = int(sys.argv[3])
             print("{:d} / {:d} = {:d}".format(a, b, div(a ,b)))
