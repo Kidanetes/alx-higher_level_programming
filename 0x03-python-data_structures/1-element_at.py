95190c7aebfc7ebeb7a7386b76526ab02172c7d5
kidanetes <kidanemaryamtesfaye@gmail.com>
2023-03-11 03:21:49 -0800

    python script

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,7 +3,7 @@ def element_at(my_list, idx):
     numOfElements = len(my_list)
     if idx < 0:
         return None
-    elif: idx >= numOfElements:
+    elif idx >= numOfElements:
         return None
     else:
         return my_list[idx]
