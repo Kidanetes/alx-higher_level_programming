3085ba04be1ea6ee2953d286df67769d086105b0
kidanetes <kidanemaryamtesfaye@gmail.com>
2023-03-11 05:18:13 -0800

    c file

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+/**
+ * is_palindrome - checks wheteher the list
+ * is palindrome or not
+ * @head: head of the list
+ * Return: 1(palindrome) or
+ * 0(not palindrome)
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *tmp = *head, *tmp;
+	int i, count = 0;
+
+	if (*head == NULL)
+		return (1);
+	while((*tmp).next != NULL)
+	{
+		tmp = (*tmp).next;
+		count++;
+	}
+	for (i = 0; i < count; i++, count--)
+	{
+		if ((**head).n != (*tmp).n)
+			return (0);
+		*head = (**head).next;
+		tmp2 = *head;
+		while ((*tmp2).next != tmp1)
+			tmp2 = (*tmp2).next;
+		tmp = tmp2;
+	}
+	return (1);
+}
+
+
+
+
