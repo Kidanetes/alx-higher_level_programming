764f9e71eb3025ea2b3cd1ac1c2221c58fa00790
kidanetes <kidanemaryamtesfaye@gmail.com>
2023-06-06 07:11:48 -0700

    printing numbers between 1 and 99 without repeating the same digits in any order twice

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(1, 100):
     if i <= 89:
-        if i // 10 > i % 10:
+        if i // 10 >= i % 10:
             continue
         if i // 10 == 8 and i % 10 == 9:
             print("{}{}".format(i // 10, i % 10), end="\n")
