f4bd40143126bac147ba7c5dc049d91960028239
kidanetes <kidanemaryamtesfaye@gmail.com>
2023-06-08 13:57:22 -0700

    fixing spelling in function definition

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-def magic_calculaton(a, b):
+def magic_calculation(a, b):
     import magic_calculation_102 as mg
     if a < b:
         res = mg.add(a, b)
-        for i in range(4,6):
+        for i in range(4, 6):
             res = mg.add(res, i)
         return res
     else:
