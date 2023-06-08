268a79134ac2173de37fb2f96107512c72388f00
kidanetes <kidanemaryamtesfaye@gmail.com>
2023-06-08 13:46:43 -0700

    Byte code

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def magic_calculaton(a, b):
+    import magic_calculation_102 as mg
+    if a < b:
+        res = mg.add(a, b)
+        for i in range(4,6):
+            res = mg.add(res, i)
+        return res
+    else:
+        return mg.sub(a, b)
