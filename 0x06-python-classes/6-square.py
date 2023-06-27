b39ff39288aadd08e8ff6bdf756d10fdf7dea090
kidanetes <kidanemaryamtesfaye@gmail.com>
2023-06-27 15:15:32 -0700

    class square with private field postion

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -39,10 +39,12 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if type(value) is tuple and value[0] >= 0 and value[1] >= 0:
-            self.__position = value
-        else:
+        if type(value) is not tuple or type(value[0]) is not int or
+        type(value[1]) is not int:
             raise TypeError("position must be a tuple of 2 positive integers")
+        if len(value) != 2 or value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
 
     def area(self):
         """calculate the area of a square
