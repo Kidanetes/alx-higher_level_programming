e35de0160abb440ffbc869d7e333ea8abd3b1a45
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-07-01 14:10:29 +0300

    using for loop with else

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,24 +1,19 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from sys import argv, exit
-    from calculator_1 import add, sub, mul, div
-    num = len(argv) - 1
-    if num != 3:
+if __name__ == '__main__':
+    from calculator_1 import add, sub, div, mul
+    import sys
+    if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
+        sys.exit(1)
+    op = ["+", "-", "*", "/"]
+    a = int(sys.argv[1])
+    op_s = sys.argv[2]
+    b = int(sys.argv[3])
+    fun = [add, sub, mul, div]
+    for i in range(len(op)):
+        if op[i] == op_s:
+            print("{} {} {} = {}".format(a, op_s, b, fun[i](a,b)))
+            sys.exit(0)
     else:
-        a = int(argv[1])
-        b = int(argv[3])
-        op = argv[2]
-        if op == "+":
-            print("{} {} {} = {}".format(a, op, b, add(a, b)))
-        elif op == "-":
-            print("{} {} {} = {}".format(a, op, b, sub(a, b)))
-        elif op == "*":
-            print("{} {} {} = {}".format(a, op, b, mul(a, b)))
-        elif op == "/":
-            print("{} {} {} = {}".format(a, op, b, div(a, b)))
-        else:
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
-    exit(0)
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
