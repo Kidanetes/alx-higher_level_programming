9e8e747328cab5fb341cfbe97bb03c4524d4b551
kidanetes <kidanemaryamtesfaye@gmail.com>
2023-07-03 14:39:02 -0700

    add a class method which crate a square object

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -120,8 +120,4 @@ class Rectangle:
         size(int): size of the square
         Returns: new square object
         """
-        if type(size) is not int:
-            raise TypeError('width must be an integer')
-        if size < 0:
-            raise ValueError('width must be >= 0')
         return cls(size, size)
