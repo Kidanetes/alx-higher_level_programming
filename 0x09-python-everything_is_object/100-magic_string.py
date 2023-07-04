51f908b47b7c9c420769af14867bd4302410c68b
kidanetes <kidanemaryamtesfaye@gmail.com>
2023-07-04 12:21:07 -0700

    using List behaviour to print magic string

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def magic_string(my_list=[]):
-    my_list.append("BestSchool") if len(my_list) == 0 else my_list.append(", BestSchool")
+    my_list.append("BestSchool") if len(my_list) == 0\
+                else my_list.append(", BestSchool")
     return "".join(my_list)
