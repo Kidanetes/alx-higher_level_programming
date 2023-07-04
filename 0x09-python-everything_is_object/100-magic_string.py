5da7acc578413afe4dc6ee31fe460f8759fc76e0
kidanetes <kidanemaryamtesfaye@gmail.com>
2023-07-04 12:37:54 -0700

    pycodestyle fix

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(my_list=[]):
-    my_list.append("BestSchool") if len(my_list) == 0 else my_list.append(", BestSchool")
+def magic_string(my_list=["BestSchool"]):
+    my_list.append(", BestSchool") if len(my_list) != 1 else my_list.append("")
     return "".join(my_list)
