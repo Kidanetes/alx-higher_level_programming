bdc04f89dc12fb72430edbcb0a46b4ac0c844ac9
kidanetes <kidanemaryamtesfaye@gmail.com>
2023-07-10 12:59:39 -0700

    Rectangle class, wic inerites from Base Geometry

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This Module contains the Rectangle Class"""
+
+
+class Rectangle(7-base_geometry.BaseGeometry):
+    """class rectangle, a sub class of Base Geometry"""
+
+    def __init__(self, width, height):
+        """intiaization a Rectangle object
+        Args:
+        width(int): width
+        height(int): height
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__heigt = height
