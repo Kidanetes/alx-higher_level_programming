5be155147cb679df517afa6933420c8b5d3e6900
kidanetes <kidanemaryamtesfaye@gmail.com>
2023-07-10 06:12:56 -0700

    subclass of list

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+"""this module contains a function which
+is called lookup"""
+
+
 def lookup(obj):
+    """
+    this functions returns list of attributes
+    of an object
+    Args:
+    obj(object): any object
+    returns: list of attributes
+    """
     return dir(obj)
-    #return list(type(obj).__dict__)
---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""this module contains a mylist class which is
+a subclass of List class"""
+
+
+class MyList(list):
+    """Mylist is a subclass of List"""
+    def print_sorted(self):
+        """this method will sort a list"""
+        l1 = self.copy()
+        l1.sort()
+        print(l1)
