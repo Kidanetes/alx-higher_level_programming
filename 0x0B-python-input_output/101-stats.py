112b9a0f4e94a7b83e86580170c8d927fcffaf3a
kidanetes <kidanemaryamtesfaye@gmail.com>
2023-07-12 06:14:42 -0700

    pycodestyle fix

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""a script that reads stdin line by line
+and computes metrics"""
+
+
+if __name__ == "__main__":
+    import sys
+
+    size = 0
+    dict_x = {}
+    status_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+    count = 0
+    size = 0
+
+    try:
+        for i in sys.stdin:
+            i = i.split()
+            try:
+                if i[-2] in status_codes:
+                    if i[-2] in dict_x:
+                        dict_x[i[-2]] += 1
+                    else:
+                        dict_x[i[-2]] = 1
+                size = size + int(i[-1])
+            except (TypeError, ValueError, IndexError):
+                pass
+            count += 1
+            if count == 10:
+                print("File size:", size)
+                for j in sorted(dict_x):
+                    print(j, dict_x[j])
+                    dict_x = {}
+                    count = 0
+    except KeyboardInterrupt:
+        print("File size:", size)
+        for j in sorted(dict_x):
+            print(j, dict_x[j])
+        raise
