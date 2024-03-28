6257b29c2656631c520e2742f46f4fddf9e2eacd
kidanetes <kidanemaryamtesfaye@gmail.com>
2024-03-28 13:39:59 +0300

    change the charset to utf-8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,4 @@
 -- change the charset of the database to UTF-8
-ALTER DATABASE htbn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE hbtn_0c_0;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
