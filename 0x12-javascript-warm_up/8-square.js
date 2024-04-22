5ff99b4e0acdd241bb34e9caacf063521406a451
kidanetes <kidanemaryamtesfaye@gmail.com>
2024-04-22 19:08:49 +0300

    print ssquare

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const arr = process.argv;
+let i = 0;
+if (arr[2] === undefined || isNaN(Number(arr[2]))) {
+  console.log('Missing size');
+} else {
+  const x = Number(arr[2]);
+  while (i < x) {
+    let str1 = '';
+    let j = 0;
+    while (j < x) {
+      str1 += 'x';
+      j++;
+    } console.log(str1);
+    i++;
+  }
+}
