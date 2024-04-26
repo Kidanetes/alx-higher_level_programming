73bcb33093dc721bc7dd45789580365d7aac87cd
kidanetes <kidanemaryamtesfaye@gmail.com>
2024-04-26 13:28:59 +0300

    select names start with N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""this module contain a python script which
+connects to the MySQLdb and run a query
+which selects names start with N"""
+
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    MY_HOST = "localhost"
+    MY_USER = argv[1]
+    MY_PASS = argv[2]
+    MY_DB = argv[3]
+    db = MySQLdb.connect(host=MY_HOST, user=MY_USER,
+                         passwd=MY_PASS, db=MY_DB, port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
+    l1 = cur.fetchall()
+    for i in l1:
+        print(i)
+    cur.close()
+    db.close()
