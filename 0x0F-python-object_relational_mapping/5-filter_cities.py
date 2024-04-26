ad77b1531e53019a7475bbaf7a52341b098f497c
kidanetes <kidanemaryamtesfaye@gmail.com>
2024-04-26 16:46:13 +0300

    adding Binary to enable case sensitivity

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     cur = db.cursor()
     cur.execute("SELECT cities.name FROM cities \
                 INNER JOIN states ON cities.state_id = states.id \
-                WHERE states.name = %s;", (arg_name, ))
+                WHERE states.name = BINARY %s;", (arg_name, ))
     l1 = cur.fetchall()
     for i in range(len(l1)):
         if i != len(l1) - 1:
