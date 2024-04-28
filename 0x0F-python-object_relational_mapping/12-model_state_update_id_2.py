06a78586fc3759110d8c50fb40245aa0405aa927
kidanetes <kidanemaryamtesfaye@gmail.com>
2024-04-28 09:52:39 +0300

    updating a row

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""this module conatins a script which will
+select all states from states table"""
+
+
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sys import argv
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+                           argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for i in session.query(State).filter(State.id == 2):
+        i.name = 'New Mexico'
+        break
+    session.commit()
