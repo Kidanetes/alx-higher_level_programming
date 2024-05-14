713acaba8cdf9b6964105fbc1619d9d661802bfe
kidanetes <kidanemaryamtesfaye@gmail.com>
2024-05-14 13:10:39 -0700

    finding peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""find a peak from a list"""
+
+
+def find_peak(list_of_integers):
+    if len(list_of_integers) == 0:
+       return None
+    for i in range(len(list_of_integers)):
+        if i > 0:
+            if 
+    return list_of_integers[-1]
