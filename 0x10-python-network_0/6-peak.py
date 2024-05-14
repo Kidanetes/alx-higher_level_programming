f94d5030a7b093469dd6d75aca70886ca084a358
kidanetes <kidanemaryamtesfaye@gmail.com>
2024-05-14 13:16:10 -0700

    finding a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -9,7 +9,5 @@ def find_peak(list_of_integers):
     """
     if len(list_of_integers) == 0:
        return None
-    for i in range(len(list_of_integers)):
-        if i > 0:
-            if 
+    list_of_integers.sort() 
     return list_of_integers[-1]
