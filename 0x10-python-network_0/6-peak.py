67cc0d4499a72f7186737d0aed75d03ca81b8cb2
kidanetes <kidanemaryamtesfaye@gmail.com>
2024-05-14 13:21:26 -0700

    pycodestyle fix

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,6 +8,6 @@ def find_peak(list_of_integers):
     list_of_integers(list)
     """
     if len(list_of_integers) == 0:
-       return None
-    list_of_integers.sort() 
+        return None
+    list_of_integers.sort()
     return list_of_integers[-1]
