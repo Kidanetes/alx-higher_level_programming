b9f1f9313653cf732374d58e866054785acd6ae8
kidanetes <kidanemaryamtesfaye@gmail.com>
2024-06-04 20:23:18 +0300

    semitandard fix

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,7 +2,8 @@
 const request = require('request');
 const process = require('process');
 const args = process.argv;
-
+if (args.length <= 2)
+   return;
 request(args[2], function (err, response, body) {
   if (err) {
     console.log(err);
@@ -10,7 +11,7 @@ request(args[2], function (err, response, body) {
     const obj = JSON.parse(body);
     let j = 0;
     for (let i = 0; i < obj.results.length; i++) {
-      if (obj.results[i].characters.includes('https://swapi-api.alx-tools.com/api/people/-/')) {
+      if (obj.results[i].characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
         j = j + 1;
       }
     }
