f4ed2d59f932c43323c80f41e69752c3ed9b4e0c
kidanetes <kidanemaryamtesfaye@gmail.com>
2024-06-04 21:16:12 +0300

    find completed tasks

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+const request = require('request');
+const process = require('process');
+const args = process.argv;
+request(args[2], function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const obj = JSON.parse(body);
+    const dict1 = {};
+    let j;
+    for (let i = 0; i < obj.length; i++) {
+      j = '' + obj[i].userId;
+      if (j in dict1) {
+        if (obj[i].completed) {
+          dict1[j]++;
+        }
+      } else {
+        if (obj[i].completed) {
+          dict1[j] = 1;
+        } else {
+          dict1[j] = 0;
+        }
+      }
+    }
+    console.log(dict1);
+  }
+});
