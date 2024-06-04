12208ab11b479575644cebf26ac846664fe5ca62
kidanetes <kidanemaryamtesfaye@gmail.com>
2024-06-04 21:21:56 +0300

    adding error

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -4,7 +4,7 @@ const process = require('process');
 const args = process.argv;
 request(args[2], function (err, response, body) {
   if (err) {
-    console.error(err);
+    console.log(err);
   } else {
     const obj = JSON.parse(body);
     const dict1 = {};
@@ -12,11 +12,11 @@ request(args[2], function (err, response, body) {
     for (let i = 0; i < obj.length; i++) {
       j = '' + obj[i].userId;
       if (j in dict1) {
-        if (obj[i].completed) {
+        if (obj[i].completed === true) {
           dict1[j]++;
         }
       } else {
-        if (obj[i].completed) {
+        if (obj[i].completed === true) {
           dict1[j] = 1;
         } else {
           dict1[j] = 0;
