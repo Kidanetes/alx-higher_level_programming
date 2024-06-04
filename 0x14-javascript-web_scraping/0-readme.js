7973961b970b5c179feb06bc0e4d5de166e0692f
kidanetes <kidanemaryamtesfaye@gmail.com>
2024-06-04 18:14:58 +0300

    semistandard fix

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
-let fs = require("fs");
-const process = require("process");
-let args = process.argv;
-fs.readFile(args[2], function(err, data) {
-	if (err) {
-		console.log(err);
-	}
-	else
-		console.log(data.toString());
+const fs = require('fs');
+const process = require('process');
+const args = process.argv;
+fs.readFile(args[2], function (err, data) {
+  if (err) {
+    console.log(err);
+  }
+  else {
+    console.log(data.toString());
+  }
 });
