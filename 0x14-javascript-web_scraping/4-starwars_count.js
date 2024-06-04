ee8cf7653a433a9a8196e1fb8564cfa6ac458add
kidanetes <kidanemaryamtesfaye@gmail.com>
2024-06-04 19:53:18 +0300

    semistandard fix

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,20 +2,18 @@
 const request = require('request');
 const process = require('process');
 const args = process.argv;
-if (args.length >= 3)
-{
-  request(args[2], function (err, response, body) {
-    if (err) {
-      console.log(err);
-    } else {
-      const obj = JSON.parse(body);
-      let j = 0;
-      for (let i = 0; i < obj.results.length; i++) {
-        if (obj.results[i].characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
-          j = j + 1;
-        }
+
+request(args[2], function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const obj = JSON.parse(body);
+    let j = 0;
+    for (let i = 0; i < obj.results.length; i++) {
+      if (obj.results[i].characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
+        j = j + 1;
       }
-      console.log(j);
     }
-  });
-}
+    console.log(j);
+  }
+});
